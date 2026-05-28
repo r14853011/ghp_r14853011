3c51917db555a63479622756aa28ed2503fb1378
r14853011 <r14853011@ntu.edu.tw>
2026-05-28 20:58:52 +0800

    load total data

--- assignment.R
+++ assignment.R
@@ -1,4 +1,17 @@
 #Assignment 3 Research Methods 
+
+#commit 1
+# External packages used:
+# - readr
+library(readr)
+
+# Load dataset
+dengue_data <- read_csv("dengue_assignment.csv")
+
+# Preview dataset
+head(dengue_data)
+
+# commit 2
 # External packages used:
 # - readr: for reading CSV files
 # - dplyr: for data manipulation and summarization
